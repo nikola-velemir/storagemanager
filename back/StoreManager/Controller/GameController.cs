14fa50fcd0d64af3fcf88aadce3440e10573c2db
nikola-velemir <vnexa021@gmail.com>
2025-03-01 20:07:02 +0100

    [fix] layout

--- back/StoreManager/Controller/GameController.cs
+++ back/StoreManager/Controller/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using StoreManager.DTO;
+using StoreManager.Service;
 
 namespace StoreManager.Controller
 {
@@ -8,40 +9,24 @@ namespace StoreManager.Controller
     [Route("games")]
     public class GameController : ControllerBase
     {
-        List<GameDTO> games = [
-   new (
-        1,
-        "Street",
-        "Fighting",
-        19.99M,
-        new DateOnly(1992,1,2)),
-    new (
-        2,
-        "KITA",
-        "BANANA",
-        19.99M,
-        new DateOnly(1992,1,2)),
-    new (
-        3,
-        "dsadasdas",
-        "BAdsadsadasdasNANA",
-        19.99M,
-        new DateOnly(1992,1,2)),
+        private GameService _gameService;
 
-    ];
-
+        public GameController()
+        {
+            _gameService = new GameService();
+        }
 
         [HttpGet]
         [Route("")]
         public ActionResult<List<GameDTO>> getGames()
         {
-            return Ok(games);
+            return Ok(_gameService.getAll());
         }
         [HttpGet]
         [Route("{id}")]
         public ActionResult<GameDTO> getGame(int id)
         {
-            return Ok(games.FirstOrDefault(game => { return game.Id == id; }));
+            return Ok(_gameService.getById(id));
         }
     }
 }
---
+++ back/StoreManager/Service/GameService.cs
@@ -0,0 +1,39 @@
+﻿using StoreManager.DTO;
+
+namespace StoreManager.Service
+{
+    public class GameService
+    {
+        List<GameDTO> games = [
+   new (
+        1,
+        "Street",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992,1,2)),
+    new (
+        2,
+        "KITA",
+        "BANANA",
+        19.99M,
+        new DateOnly(1992,1,2)),
+    new (
+        3,
+        "dsadasdas",
+        "BAdsadsadasdasNANA",
+        19.99M,
+        new DateOnly(1992,1,2)),
+
+    ];
+        public List<GameDTO> getAll()
+        {
+            return games;
+        }
+
+        public GameDTO? getById(int id)
+        {
+            return games.FirstOrDefault(game => game.Id == id);
+        }
+
+    }
+}
