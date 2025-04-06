8123fa5d8ca32a643a9e4cc14b88b6509bc0469b
nikola-velemir <vnexa021@gmail.com>
2025-04-06 02:27:32 +0200

    [feature] paginated responses for invoices

--- front/react-app/src/components/InvoiceSearch/InvoiceSearch.tsx
+++ front/react-app/src/components/InvoiceSearch/InvoiceSearch.tsx
@@ -2,16 +2,24 @@ import React, { useEffect, useState } from "react";
 import InvoiceSearchCard from "./Cards/InvoiceSearchCard";
 import { InvoiceSearchResponse } from "../../model/invoice/InvoiceSearchResponse";
 import { InvoiceService } from "../../services/InvoiceService";
+import InvoiceSearchPagination from "./InvoiceSearchPagination";
 
 const InvoiceSearch = () => {
   const [invoices, setInvoices] = useState<InvoiceSearchResponse[]>([]);
   useEffect(() => {
-    InvoiceService.findAll().then((response) => {
-      setInvoices(response.data.responses);
+    InvoiceService.findFiltered({
+      date: null,
+      id: null,
+      pageNumber: 1,
+      pageSize: 1,
+    }).then((response) => {
+      console.log(response.data);
+      setInvoices(response.data.responses.items);
     });
   }, []);
   return (
-    <div className="h-screen w-full p-8 ">
+    <div className="h-screen r w-full p-8 ">
+      <InvoiceSearchPagination></InvoiceSearchPagination>
       <div className="h-5/6 overflow-y-auto flex items-center flex-col">
         {invoices.map((invoice: InvoiceSearchResponse) => {
           console.log(invoice);
